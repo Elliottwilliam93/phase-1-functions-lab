240a603ae3d11e624ce088efab8ffe73a5cd6c30
williamelliott <ironmaiden229@hotmail.com>
2023-03-17 10:27:23 -0400

    project finished

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+  let blocks = 0;
+  if(num > 42) {
+    blocks = num - 42;
+    } else {
+    blocks = 42 - num;
+    }
+  return blocks;
+}
+
+function distanceFromHqInFeet(num) {
+  let blocks = 0;
+  if(num > 42) {
+    blocks = num - 42;
+    } else {
+    blocks = 42 - num;
+    }
+  let feet = 264 * blocks
+  return feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+  let blocks = 0;
+  if(start > end) {
+    blocks = start - end;
+    } else {
+    blocks = end - start;
+    }
+  let feet = 264 * blocks
+  return feet;
+}
+
+function calculatesFarePrice(start, end) {
+  let blocks = 0;
+  let fare = 0;
+  if(start > end) {
+    blocks = start - end;
+    } else {
+    blocks = end - start;
+    }
+  let feet = 264 * blocks
+  if(feet <= 400){
+    return fare;
+  } else if(feet > 400 && feet <= 2000) {
+    let newFeet = feet - 400;
+    fare = newFeet * .02;
+    return fare;
+  } else if(feet > 2000 && feet <= 2500) {
+    fare = 25;
+    return fare;
+  } else if(feet > 2500) {
+    return "cannot travel that far";
+  }
+}
